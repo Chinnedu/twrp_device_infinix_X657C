f6786af37fc48eb4156264d5338ffe816ca266b3
infinixs5lite <kelvinchinedu97@gmail.com>
2023-07-02 17:47:27 +0100

    Update twrp_X657C.mk

--- twrp_X657C.mk
+++ twrp_X657C.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from X657C device
 $(call inherit-product, device/infinix/X657C/device.mk)
