bab17f76a31f3f53c0a6f29f0f12bd255eda5702
infinixs5lite <kelvinchinedu97@gmail.com>
2023-07-02 17:14:01 +0100

    Update and rename omni_Infinix-X657C.mk to twrp_X657C.mk

--- twrp_X657C.mk
+++ twrp_X657C.mk
@@ -6,18 +6,19 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk
 
-# Inherit from Infinix-X657C device
-$(call inherit-product, device/infinix/Infinix-X657C/device.mk)
+# Inherit from X657C device
+$(call inherit-product, device/infinix/X657C/device.mk)
 
-PRODUCT_DEVICE := Infinix-X657C
-PRODUCT_NAME := omni_Infinix-X657C
+PRODUCT_DEVICE := X657C
+PRODUCT_NAME := twrp_X657C
 PRODUCT_BRAND := Infinix
-PRODUCT_MODEL := Infinix X657C
+PRODUCT_MODEL := Infinix smart 5
 PRODUCT_MANUFACTURER := infinix
 
 PRODUCT_GMS_CLIENTID_BASE := android-infinix
